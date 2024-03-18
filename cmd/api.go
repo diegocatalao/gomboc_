326ce7b820a46895570f0f7308423ccb515df432
Diego Catalão <diego.catalao@gabriel.com.br>
2024-03-18 02:12:55 -0300

    fix: ignore global variables and couple them within the init function

--- cmd/api.go
+++ cmd/api.go
@@ -6,27 +6,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var host string
-var port int
-var migrate bool
-
-var apiCmd = &cobra.Command{
-	Use:   "api",
-	Short: "Start Gomboc API",
-	Long:  "Start the Gomboc API with a host (default 0.0.0.0) and a port (default 7020)",
-	Run: func(cmd *cobra.Command, args []string) {
-		// Start Gomboc API.
-		//
-		// Peer devices will register with the api. Control and management of devices will be
-		// done through the administrator user interface.
-		api.GombocAPI{Host: host, Port: port, AutoMigrate: migrate}.Initialize()
-	},
-}
-
 func init() {
-	rootCmd.AddCommand(apiCmd)
+	var host string  // The API local host address
+	var port int     // The API local port
+	var migrate bool // Sinalize automigrate for models
 
-	apiCmd.Flags().StringVarP(&host, "host", "", "0.0.0.0", "API host address")
-	apiCmd.Flags().IntVarP(&port, "port", "", 7020, "API port number")
-	apiCmd.Flags().BoolVarP(&migrate, "migrate", "", false, "Auto migrate models")
+	var cmd = &cobra.Command{
+		Use:   "api",
+		Short: "Start Gomboc API",
+		Long:  "Start the Gomboc API with a host (default 0.0.0.0) and a port (default 7020)",
+		Run: func(cmd *cobra.Command, args []string) {
+			// Start Gomboc API.
+			//
+			// Peer devices will register with the api. Control and management of devices will be
+			// done through the administrator user interface.
+			api.GombocAPI{Host: host, Port: port, AutoMigrate: migrate}.Initialize()
+		},
+	}
+
+	cmd.Flags().StringVarP(&host, "host", "", "0.0.0.0", "API host address")
+	cmd.Flags().IntVarP(&port, "port", "", 7020, "API port number")
+	cmd.Flags().BoolVarP(&migrate, "migrate", "", false, "Auto migrate models")
+
+	rootCmd.AddCommand(cmd)
 }
--- cmd/rdv.go
+++ cmd/rdv.go
@@ -1,26 +1,24 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
-var rdvHost string
-var rdvPort int
-
-var rdvCmd = &cobra.Command{
-	Use:   "rdv",
-	Short: "Start Gomboc RDV service (rendezvous server)",
-	Long:  "Start Gomboc RDV service with a host (default 0.0.0.0) and a port (default 7021)",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("rdv called")
-	},
-}
-
 func init() {
-	rootCmd.AddCommand(rdvCmd)
+	var host string // The Rendezvous server address
+	var port int    // The Rendezvous server port
 
-	rdvCmd.Flags().StringVarP(&rdvHost, "host", "", "", "RDV host address")
-	rdvCmd.Flags().IntVarP(&rdvPort, "port", "", 7020, "RDV port number")
+	var cmd = &cobra.Command{
+		Use:   "rdv",
+		Short: "Start Gomboc RDV service (rendezvous server)",
+		Long:  "Start Gomboc RDV service with a host (default 0.0.0.0) and a port (default 7021)",
+		Run: func(cmd *cobra.Command, args []string) {
+			panic("The command line for 'rdv' not implemented yet!")
+		},
+	}
+
+	cmd.Flags().StringVarP(&host, "host", "", "", "RDV host address")
+	cmd.Flags().IntVarP(&port, "port", "", 7020, "RDV port number")
+
+	rootCmd.AddCommand(cmd)
 }
